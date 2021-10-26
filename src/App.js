5dc3d85dc9036d79db4455e787cb8410c27797c0
stevenspicher <71337603+stevenspicher@users.noreply.github.com>
2021-10-26 11:38:40 -0600

    added link to site

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import info from './components/data.jsx';
+import Footer from './components/Footer';
 
 
 const App = () => {
@@ -10,7 +11,10 @@ const App = () => {
       <div className="content">
         <Home info={info} />
       </div>
-        </div>
+      <div className="footer">
+      <Footer info={info[1].link}/>
+      </div>
+    </div>
   );
 }
 
